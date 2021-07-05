bebf6e964ffbc5cc0adf1b980f10503f54b5af0a
Devarshivaidya <devarshivaidya2307@gmail.com>
2021-07-05 17:04:39 +0530

    first commit

---
+++ studio/schemas/chef.js
@@ -0,0 +1,33 @@
+export default {
+    name: "chef",
+    title: "chef",
+    type: "document", 
+    fields:[
+        {
+            name: "name",
+            title: "chef's Name",
+            type: "string",
+        },
+        {
+            name: "image",
+            title: "Image",
+            type: "image",
+            options: {
+                hotspot:true,
+            },
+        },
+        {
+            name: "bio",
+            title: "Bio",
+            type: "array",
+            of: [
+                  {
+                    title: "Block",
+                    type: "block",
+                    styles: [{title: "Normal", value:"normal"}],
+                    lists: [],
+                },
+            ],
+        },
+    ], 
+};
